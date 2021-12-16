3628895dcf796e8be6937a68e1ae743f880f14f4
Russell Waite <russellwaite@gmail.com>
2021-12-16 16:59:39 +0000

    day 12 update

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -32,19 +32,8 @@ fn create_map(path: &str) -> HashMap<String, Vec<String>> {
     let mut hashmap:HashMap<String, Vec<String>> = HashMap::with_capacity(pairs.len());
     
     for (key, value) in pairs {
-        if let Some(vector) = hashmap.get_mut(key.as_str()) {
-            let temp = value.to_string();
-            vector.push(temp);
-        } else {
-            hashmap.insert(key.to_string(), vec![value.to_string()]);
-        }
-        // if A can go to b, then b can go to A
-        if let Some(vector) = hashmap.get_mut(value.as_str()) {
-            let temp = key.to_string();
-            vector.push(temp);
-        } else {
-            hashmap.insert(value.to_string(), vec![key.to_string()]);
-        }
+        hashmap.entry(key.clone()).or_insert_with(Vec::new).push(value.clone());
+        hashmap.entry(value.clone()).or_insert_with(Vec::new).push(key.clone()); 
     }
     hashmap
 }
@@ -55,9 +44,8 @@ fn day12_1_result(path: &str) -> usize {
     paths.len()
 }
 
-fn travel(map: &HashMap<String, Vec<String>>, start: &str, end: &str, visited: Vec<String>) -> Vec<Vec<String>> {
-    let mut local_visited: Vec<String>  = visited.clone(); 
-    local_visited.push(start.to_string());
+fn travel(map: &HashMap<String, Vec<String>>, start: &str, end: &str, mut visited: Vec<String>) -> Vec<Vec<String>> {
+    visited.push(start.to_string());
 
     let caves = match map.get(start) {
         Some(caves) => caves,
@@ -65,17 +53,17 @@ fn travel(map: &HashMap<String, Vec<String>>, start: &str, end: &str, visited: V
     };
     let mut journeys: Vec<Vec<String>> = vec![];
 
-    for cave in caves {
-        if local_visited.contains(cave) && cave.to_lowercase() == *cave {
+    for cave in caves.iter()  {
+        if visited.contains(cave) && cave.to_lowercase() == *cave {
             continue; // not allowed to visit small cave twice
         }
         if cave == end { //short circtuit another trip, we know this is the end...
-            let mut copy = local_visited.clone();
+            let mut copy = visited.clone();
             copy.push(cave.to_string());
             journeys.push(copy);
             continue;
         }
-        journeys.append(&mut travel(map, cave, end, local_visited.clone()));
+        journeys.append(&mut travel(map, cave, end, visited.clone()));
     }
 
     journeys
@@ -86,6 +74,10 @@ fn day12_2_result(path: &str) -> usize {
     let hashmap = create_map(path);
     let mut paths:Vec<Vec<String>> = vec![];
     let banned_specials = ["start".to_string(), "end".to_string()];
+    let small_caves: Vec<String> = hashmap.keys()
+        .filter(|&key| key.to_lowercase() == *key)
+        .map(|key| key.to_owned())
+        .collect();
     //let special_cases: Vec<String> = 
     hashmap
         .keys()
@@ -93,7 +85,7 @@ fn day12_2_result(path: &str) -> usize {
         .filter(|&s| s.to_lowercase() == *s)
         .for_each(|special| {
             println!("Today's special is {}", &special);
-            paths.append(&mut travel2(&hashmap, "start", "end", vec![], (special, 0)));
+            paths.append(&mut travel2(&hashmap, "start", "end", vec![], special, &small_caves));
         });
     let mut distinct:Vec<Vec<String>> = vec![];
 
@@ -105,10 +97,8 @@ fn day12_2_result(path: &str) -> usize {
     distinct.len()
 }
 
-fn travel2(map: &HashMap<String, Vec<String>>, start: &str, end: &str, visited: Vec<String>, special: (&str, u8)) -> Vec<Vec<String>> {
-    let mut local_visited: Vec<String>  = visited.clone(); 
-    local_visited.push(start.to_string());
-    let mut local_special = special.clone();
+fn travel2(map: &HashMap<String, Vec<String>>, start: &str, end: &str, mut visited: Vec<String>, special: &str, small_caves: &Vec<String>) -> Vec<Vec<String>> {
+    visited.push(start.to_string());
     let mut journeys: Vec<Vec<String>> = vec![];
 
     let caves = match map.get(start) {
@@ -117,8 +107,8 @@ fn travel2(map: &HashMap<String, Vec<String>>, start: &str, end: &str, visited: 
     };
 
     for cave in caves {
-        if cave == local_special.0 &&
-            local_visited.iter().filter(|cave| cave.as_str() == local_special.0).count() > 1 {
+        if cave == special &&
+            visited.iter().filter(|cave| cave.as_str() == special).count() > 1 {
                 continue;
             // I think this is experiencing threading issues, debuigging was skipping all over like
             // it does in VS/Rider when on multi-threaded code.
@@ -127,16 +117,16 @@ fn travel2(map: &HashMap<String, Vec<String>>, start: &str, end: &str, visited: 
             //}
             //local_special.1 += 1;
         }
-        if cave != local_special.0 && local_visited.contains(cave) && cave.to_lowercase() == *cave {
+        if cave != special && visited.contains(cave) && small_caves.contains(cave) {
             continue; // not allowed to visit small cave twice
         }
         if cave == end { //short circtuit another trip, we know this is the end...
-            let mut copy = local_visited.clone();
+            let mut copy = visited.clone();
             copy.push(cave.to_string());
             journeys.push(copy);
             continue;
         }
-        journeys.append(&mut travel2(map, cave, end, local_visited.clone(), local_special.clone()));
+        journeys.append(&mut travel2(map, cave, end, visited.clone(), special, small_caves));
     }
 
     journeys
